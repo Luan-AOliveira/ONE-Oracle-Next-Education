42fe8a99325904fc0e2ca695986eec3a781ac9ed
Luan Alves Oliveira <144074045+Luan-AOliveira@users.noreply.github.com>
2025-01-23 19:03:54 -0300

    Update Jogo número secreto JS

---
+++ JS-Jogo-Número-Secreto/app.js
@@ -0,0 +1,14 @@
+// Seleciona o elemento h1 e altera o seu texto
+let titulo = document.querySelector('h1');
+
+// Altera o texto do título para 'Jogo do Número Secreto'
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+// Seleciona o elemento p e altera o seu texto
+let paragrafo = document.querySelector('p');
+
+// Altera o texto do parágrafo para 'Escolha um número entre 1 e 10'
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+
+verificaChute();

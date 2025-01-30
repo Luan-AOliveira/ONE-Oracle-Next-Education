193d62073c464b43ea893750c586fd2b20bd8507
Luan Alves Oliveira <144074045+Luan-AOliveira@users.noreply.github.com>
2025-01-30 00:42:07 -0300

    Projeto JS Jogo Numero Secreto Concluido

--- Projetos/JS-Jogo-Número-Secreto/app.js
+++ Projetos/JS-Jogo-Número-Secreto/app.js
@@ -16,10 +16,33 @@ let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 
+
 // Criando uma função para reutilizar várias vezes sem precisar toda vez criar como no código acima
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
+}
+
+// Criando uma função para falar os textos iniciais
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 
